ba166747998500c77c209537d5331ab74746cf44
Mduduzi Keswa <mduduzi.keswa@bconv.com>
2012-08-21 21:02:01 -0400

    Clean up :(

--- com.conx.logistics.kernel/com.conx.logistics.kernel.ui/kernel.ui.editors/builder/src/main/java/com/conx/logistics/kernel/ui/editors/builder/IFormFactory.java
+++
@@ -1,7 +0,0 @@
-package com.conx.logistics.kernel.ui.editors.builder;
-
-import com.conx.logistics.kernel.ui.components.domain.form.ConXForm;
-
-public interface IFormFactory {
-	public Object create(ConXForm arg0);
-}
--- com.conx.logistics.kernel/com.conx.logistics.kernel.ui/kernel.ui.forms/vaadin/src/main/java/com/conx/logistics/kernel/ui/forms/vaadin/IVaadinForm.java
+++
@@ -1,8 +0,0 @@
-package com.conx.logistics.kernel.ui.forms.vaadin;
-
-import com.vaadin.data.Item;
-import com.vaadin.ui.Component;
-
-public interface IVaadinForm extends Component {
-	public void setItemDataSource(Item item);
-}
